65d9351333f60d6a992e018f603329147aff9a60
Jason Hyde <github@2bad.me>
2024-07-03 06:54:00 +0200

    [ts] reexport setup function from main entrypoint

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,7 @@ import type { ESLint } from 'eslint'
 import all from './configs/all.ts'
 import recommended from './configs/recommended.ts'
 import strict from './configs/strict.ts'
+export { setup } from './setup.ts'
 
 const plugin: ESLint.Plugin = {
   configs: {
