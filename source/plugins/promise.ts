9de27f6a8bc590d2469b42b4789f1d9be4010ed7
Jason Hyde <github@2bad.me>
2024-09-01 04:27:20 +0200

    [ts] set `promise/prefer-await-*` rules to `error`

--- source/plugins/promise.ts
+++ source/plugins/promise.ts
@@ -5,7 +5,11 @@ import plugin from 'eslint-plugin-promise'
 export const config: Linter.FlatConfig[] = [
   {
     name: '2bad/eslint-plugin-promise',
-    ...plugin.configs['flat/recommended']
+    ...plugin.configs['flat/recommended'],
+    rules: {
+      'promise/prefer-await-to-callbacks': 'error',
+      'promise/prefer-await-to-then': ['error', { strict: true }]
+    }
   }
 ]
 
