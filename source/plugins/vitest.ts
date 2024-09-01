5658649e97d29ae1a03612f3826e78d82a91dc05
Jason Hyde <github@2bad.me>
2024-09-01 06:18:43 +0200

    [ts] update vitest plugin to support type testing and recommended rules

--- source/plugins/vitest.ts
+++ source/plugins/vitest.ts
@@ -4,12 +4,23 @@ import type { Linter } from 'eslint'
 export const config: Linter.FlatConfig[] = [
   {
     name: '2bad/eslint-plugin-vitest',
+    ...plugin.configs.all,
+    ...plugin.configs.env,
     files: ['**/*.test.ts'],
-    plugins: {
-      plugin
+    // https://vitest.dev/guide/testing-types
+    settings: {
+      vitest: {
+        typecheck: true
+      }
+    },
+    languageOptions: {
+      globals: {
+        ...plugin.environments.env.globals
+      }
     },
     rules: {
-      ...plugin.configs.all.rules,
+      // this rules are not part of 'all' preset for some reason
+      ...plugin.configs.recommended.rules,
       'vitest/max-nested-describe': ['error', { max: 3 }],
       'vitest/no-hooks': ['error', { allow: ['afterEach'] }]
     }
