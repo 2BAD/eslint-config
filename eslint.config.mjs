5a384b845df0194aae2d8c8a6fc8223b43cab44f
Jason Hyde <github@2bad.me>
2024-09-01 05:04:25 +0200

    [ts] simplify example config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,4 @@
-import * as tb from './build/index.js'
-import * as plugin from './build/plugins/index.js'
+import * as tb from '@2bad/eslint-config'
+import * as plugins from '@2bad/eslint-config/plugins'
 
-export default [
-  ...tb.setup(import.meta.dirname),
-  ...tb.configs.strict,
-  ...plugin.promise,
-  ...plugin.import,
-  ...plugin.jsdoc
-]
+export default [tb.setup(import.meta.dirname), tb.configs.strict, ...Object.values(plugins)].flat()
