c0b79c4d938a363864e453b7d68cd16965df872c
Jason Hyde <github@2bad.me>
2024-09-01 07:36:04 +0200

    [ts] get only the rules that we need from neostandard

--- source/plugins/neostandard.ts
+++ source/plugins/neostandard.ts
@@ -1,6 +1,20 @@
 import type { Linter } from 'eslint'
 import neostandard from 'neostandard'
 
-export const config: Linter.Config[] = [...neostandard({ ts: true })]
+const ns = neostandard({ ts: true })
+
+const standardRules = ns.flatMap((config) => {
+  if (!config.rules) return {}
+
+  // Filter out rules that start with 'n/'
+  const filteredRules = Object.fromEntries(
+    Object.entries(config.rules).filter(([key]) => !key.startsWith('n/') && !key.startsWith('react/'))
+  )
+
+  if (Object.keys(config.plugins ?? []).includes('@stylistic')) return config
+  return { rules: filteredRules }
+})
+
+export const config: Linter.Config[] = standardRules
 
 export default config
