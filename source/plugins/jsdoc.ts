a976a4dcead4ceb2668b0f91439425ae0c3545e2
Jason Hyde <github@2bad.me>
2024-09-01 06:00:08 +0200

    [conf] set `jsdoc` plugin to `recommended-typescript`

--- source/plugins/jsdoc.ts
+++ source/plugins/jsdoc.ts
@@ -1,6 +1,6 @@
 import type { Linter } from 'eslint'
 import jsdoc from 'eslint-plugin-jsdoc'
 
-export const config: Linter.FlatConfig[] = [jsdoc.configs['flat/recommended']]
+export const config: Linter.FlatConfig[] = [jsdoc.configs['flat/recommended-typescript']]
 
 export default config
