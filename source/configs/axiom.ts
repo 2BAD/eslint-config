df39e64f9d9afbb3ce4bcd45a0914bae995723b1
Jason Hyde <github@2bad.me>
2024-09-14 10:05:33 +0200

    [conf] enable `@typescript-eslint/prefer-for-of` and prefer private fields

--- source/configs/axiom.ts
+++ source/configs/axiom.ts
@@ -4,10 +4,21 @@ const axiom: Linter.Config[] = [
   {
     name: 'axiom/rules/all',
     rules: {
-      // see: https://github.com/standard/standard/issues/1464
-      // see: https://github.com/eslint/eslint/pull/12613
+      // Reason: https://github.com/standard/standard/issues/1464
+      // Reason: https://github.com/eslint/eslint/pull/12613
       'no-void': ['error', { allowAsStatement: true }],
-      '@typescript-eslint/no-floating-promises': 'error'
+      '@typescript-eslint/no-floating-promises': 'error',
+      // Reason: for..of works on any iterable
+      // Reason: for..of supports all kinds of control flow in the loop body, like `continue`, `break`, `return`, `yield` and `await`.
+      '@typescript-eslint/prefer-for-of': 'error',
+      // Reason: prefer runtime private fields over compile time private properties
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: ':matches(PropertyDefinition, MethodDefinition)[accessibility="private"]',
+          message: 'Use #private fields instead'
+        }
+      ]
     }
   }
 ]
